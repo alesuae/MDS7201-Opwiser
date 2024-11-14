93a8e63fd6ee73d67b2ccdb056c2c66f8afceeb7
Alejandra Toro <alejandra.toro@ug.uchile.cl>
2024-11-14 00:44:15 -0300

    fix: updated get method for bcchile

--- src/data/utils/data_downloader.py
+++ src/data/utils/data_downloader.py
@@ -1,23 +1,53 @@
 # TODO: check BancoCentral api connections
-
-import requests
+import bcchapi
 import pandas as pd
+import numpy as np
+from config import get_config
 
-def download_data_from_api(api_url: str, params=None, api_key:str = None) -> pd.DataFrame:
+def download_data_from_api(config: str) -> pd.DataFrame:
     """
     Downloads data from BancoCentral API and returns it as a DataFrame
 
-    :param api_url: API URL.
-    :param params: GET request parameters.
-    :param api_key: API key, if required.
-    :return: DataFrame with the downloaded data.
+    Args:
+        config (str): Configuration file name
+    Returns:
+        DataFrames with the downloaded data
     """
-    headers = {'Authorization': f'Bearer {api_key}'} if api_key else {}
-    response = requests.get(api_url, headers=headers, params=params)
+    config_dict = get_config(config)
 
-    if response.status_code == 200:
-        data = response.json()
-        return pd.DataFrame(data)  # Transforms the data into a DataFrame
+    if "pib" in config_dict["series"].keys():
+        pib_series = config_dict["series"].pop("pib")
+        monthly_series = config_dict["series"]
     else:
-        print(f"Error retrieving data: {response.status_code}")
-        return pd.DataFrame()  # Returns an empty DataFrame in case of error
+        monthly_series = config_dict["series"]
+
+    try:
+        siete = bcchapi.Siete(file=config_dict["creds"])
+    except:
+        print("Error: Could not connect to the BancoCentral API")
+        return pd.DataFrame()
+
+    start = config_dict["dates"]["start"]
+    end = config_dict["dates"]["end"]
+
+    exog_monthly = siete.cuadro(
+        series=list(monthly_series.values()),
+        nombres=list(monthly_series.keys()),
+        desde=start,
+        hasta=end,
+        variacion=12,
+        frecuencia="M",
+        observado={key: np.mean for key in monthly_series.keys()},
+    )
+
+    exog_quarterly = siete.cuadro(
+        series=[pib_series],
+        nombres=["pib"],
+        desde=start,
+        hasta=end,
+        frecuencia="Q",
+        observado={"pib": "last"},
+    )
+
+    assert type(exog_monthly) == pd.DataFrame and type(exog_quarterly) == pd.DataFrame
+    return exog_monthly.dropna(), exog_quarterly.dropna()
