020947e135c469d279615e22d72d6e4bb7be1a39
Alejandra Toro <alejandra.toro@ug.uchile.cl>
2024-11-15 01:06:40 -0300

    fix: updated preprocessing of sales and stock data

--- src/data/dataset_makers/stock_dataset_maker.py
+++ src/data/dataset_makers/stock_dataset_maker.py
@@ -1,11 +1,26 @@
 from base.base_dataset_maker import BaseDatasetMaker
+from base.progress_bar import ProgressBar
+from utils.config import get_config
 import pandas as pd
 
 
 class StockDatasetMaker(BaseDatasetMaker):
+    def __init__(self, config_mode: str):
+        self.config_mode = config_mode
+        self.progress_bar = ProgressBar()
+
+        self.config_dict = get_config(self.config_mode)
+        self.source_path = self.config_dict['stock']["path"]
+
+        self.numeric_variables = self.config_dict["stock"]["numeric_vars"]
+        self.sku = self.config_dict["sku"]
+
     def load_data(self):
+        self.progress_bar.update_total_steps(1)
+        self.progress_bar.log("Loading stock data...")
         self.data = pd.read_csv(self.source_path)
-        print("Stock data loaded.")
+        self.progress_bar.check()
+        self.progress_bar.close()
 
     def clean_data(self):
         # TODO: implement data imputation of missing values
@@ -13,6 +28,10 @@ class StockDatasetMaker(BaseDatasetMaker):
         print("Stock data cleaned.")
 
     def transform_data(self):
-        # TODO: check units of measurement
-        # Example transformation to unify units of measurement, if necessary
-        print("Stock data transformed.")
+        variables = list(self.numeric_variables)
+
+        self.progress_bar.log("Transforming stock data to numeric")
+        for idx in range(len(variables)):
+            self.data[variables[idx]] = pd.to_numeric(self.data[variables[idx]].str.replace(',', ''), errors='raise')
+        self.progress_bar.check()
+        self.progress_bar.close()
