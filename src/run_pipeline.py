9afaef24010f4dc8be59ef883ebea50c5f56f7fd
Alejandra Toro <alejandra.toro@ug.uchile.cl>
2024-11-16 18:46:30 -0300

    FIX: fix model pipeline

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,23 +1,19 @@
 from src.data.data_preprocessing.prepare_data import DataPreparer
 from src.data.data_preprocessing.data_splitter import DataSplitter
+# TODO: HACERLOOOOOOOAAAAAA
 from src.models.lightgbm_model import LightGBMModel
 from src.models.arima_model import ARIMAModel
-from utils.yaml_reader import load_yaml
+from src.models.utils.model_config import get_config
 
 # Cargar configuraciones
-model_config = load_yaml("model.config.yaml")
+model_config = get_config('model')
 
 # Seleccionar modelo
 model_name = "LightGBM"  # Cambia esto dinámicamente según el flujo
 model_settings = model_config['models'][model_name]
 
-# Preparar datos
-preparer = DataPreparer(config=model_settings['preprocesamiento'])
-df_prepared = preparer.prepare(df_combined)
-
-# Dividir datos
-splitter = DataSplitter(config=model_settings)
-splits = splitter.split(df_prepared, target='venta_total_neto')
+from src.data.main import data_pipeline
+X_train, X_val, X_test, y_train, y_val, y_test = data_pipeline()
 
 # Entrenar modelo
 if model_name == "LightGBM":
@@ -26,4 +22,4 @@ elif model_name == "ARIMA":
     model = ARIMAModel(params=model_settings['hyperparameters'])
 
 # Usar los splits para entrenar/validar
-model.train(*splits[:2])  # Entrenamiento con X_train, y_train
+model.train(X_train, y_train)  # Entrenamiento con X_train, y_train
