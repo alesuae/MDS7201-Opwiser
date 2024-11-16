df9bc677d14965083858e3012b11d921ba2a8f3b
Alejandra Toro <alejandra.toro@ug.uchile.cl>
2024-11-16 18:42:46 -0300

    fix: read dataset with polars

--- src/data/dataset_makers/stock_dataset_maker.py
+++ src/data/dataset_makers/stock_dataset_maker.py
@@ -2,7 +2,7 @@ from base.base_dataset_maker import BaseDatasetMaker
 from base.progress_bar import ProgressBar
 from utils.config import get_config
 import pandas as pd
-
+import polars as pl
 
 class StockDatasetMaker(BaseDatasetMaker):
     def __init__(self, config_mode: str):
@@ -12,22 +12,23 @@ class StockDatasetMaker(BaseDatasetMaker):
         self.config_dict = get_config(self.config_mode)
         self.source_path = self.config_dict['stock']["path"]
 
-        self.numeric_variables = self.config_dict["stock"]["numeric_vars"]
+        self.numeric_variables = self.config_dict["stock"]["numeric_vars"].values()
         self.sku = self.config_dict["sku"]
 
     def load_data(self):
         self.progress_bar.update_total_steps(1)
         self.progress_bar.log("Loading stock data...")
-        self.data = pd.read_csv(self.source_path)
+        self.data = pl.read_csv(source=self.source_path, separator=';')
+        self.data = self.data.to_pandas()
         self.progress_bar.check()
         self.progress_bar.close()
 
-    def clean_data(self):
-        # TODO: implement data imputation of missing values
-        self.data.fillna(0, inplace=True)
-        print("Stock data cleaned.")
+    #def clean_data(self):
+    #    # TODO: implement data imputation of missing values
+    #    self.data.fillna(0, inplace=True)
+    #    print("Stock data cleaned.")
 
-    def transform_data(self):
+    def clean_data(self):
         variables = list(self.numeric_variables)
 
         self.progress_bar.log("Transforming stock data to numeric")
