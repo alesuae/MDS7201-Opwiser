244e61dc2180621bbd172d7be88d214c27aea7a9
Alejandra Toro <alejandra.toro@ug.uchile.cl>
2024-12-11 01:18:59 -0300

    fix(integrator): merge of sales and stock by grouping dates and sku

--- src/data/main.py
+++ src/data/main.py
@@ -7,6 +7,8 @@ from src.data.data_integrator.data_aggregator import DataAggregator
 from src.data.data_preprocessing.prepare_data import DataPreparer
 from src.data.data_preprocessing.data_splitter import DataSplitter
 
+from ydata_profiling import ProfileReport
+
 def data_pipeline():
     # Create and process the sales and stock datasets
     sales_maker = SalesDatasetMaker(config_mode='data')
@@ -28,11 +30,15 @@ def data_pipeline():
     exogenous_data = exogenous_extractor.join_data()
     selector = ExogenousDataSelector(exogenous_data=exogenous_data)
     exog_selected_data = selector.select(exogenous_data)
-    print(exog_selected_data.head())
+    print('termine exog')
 
     # Integrate the datasets
+    print('emepece integrator')
     integrator = DataIntegrator(config_mode='data')
     dataset = integrator.integrate(sales_data, stock_data, exog_selected_data)
+    print('y aqui?')
+    profile_pp = ProfileReport(dataset, title="Profiling Report")
+    profile_pp.to_file("your_report_int.html")
 
     # Aggregate data (config in data.config.yaml)
     aggregator = DataAggregator(config_mode='data')
